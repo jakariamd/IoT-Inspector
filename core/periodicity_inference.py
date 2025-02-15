c7bfd4791cc84f86258e740802676f14faec1cd9
Md Jakaria <jakaria.dcc11@gmail.com>
2025-02-15 13:34:00 -0500

    periodiciry inference cleaned

--- core/periodicity_inference.py
+++ core/periodicity_inference.py
@@ -56,10 +56,15 @@ def periodic_inference(device_mac_addr):
         common.log(f'[Periodic Inference] Error reading idle data for device: {device_mac_addr}: {str(e)}')
         return
     
+    # Call the helper function to infer periodicity in network traffic
+    print(f'[Periodic Inference] Inferring periodicity for device: {device_mac_addr}')
     periodic_inference_helper(device_mac_addr, idle_data)
+    print(f'[Periodic Inference] Done inferring periodicity for device: {device_mac_addr}')
+
+    
 
 
-def fingerprint_generation():
+def fingerprint_generation(device_mac_addr):
     return
 
 def periodic_inference_helper(device_mac_addr, data):
@@ -69,7 +74,7 @@ def periodic_inference_helper(device_mac_addr, data):
         data: A pandas DataFrame containing the idle data.
     """
 
-    # get data
+    # get data from the idle data
     nums = data['network_total'].values
     times = data['start_time'].values
     protocols = data['protocol'].values
@@ -93,18 +98,19 @@ def periodic_inference_helper(device_mac_addr, data):
                 protocols[i] = ' & '.join(tmp)
                 # print(protocols[i])
         protocol_set = set(protocols)
-        print(f'Protocol Set: {protocol_set}')
+        print(f'[Peridicity Inference] Protocol Set: {protocol_set}')
         
         for i in range(len(hosts)):
             if hosts[i] != '' and hosts[i] != None:
-                tmp = hosts[i].split(';')
+                tmp = hosts[i].replace('?', '').split(';')
                 hosts[i] = tmp[0]
             if hosts[i] == None:
                 hosts[i] == 'non'
             hosts[i] = hosts[i].lower()
                 # print(hosts[i])
         domain_set = set(hosts)
-        print(f'Domain Set: {domain_set}')
+        print(f'[Peridicity Inference] Domain Set: {domain_set}')
+    # pre-processing end
 
     """
     Set Sampling Rate. In IMC23 paper, the sampling rate is set as 1 and 7200
@@ -116,7 +122,7 @@ def periodic_inference_helper(device_mac_addr, data):
     times = list(map(int,times))
     max_time = np.max(times)
     min_time = np.min(times)
-    print(f'Max Time: {max_time}, Min Time: {min_time}')
+    print(f'[Peridicity Inference] Max Time: {max_time}, Min Time: {min_time}')
 
 
     """
@@ -133,7 +139,6 @@ def periodic_inference_helper(device_mac_addr, data):
         """
         merge domain names with the same suffix
         """
-
         for i in cur_domain_set.copy():
             matched = 0
             if len(i.split('.')) >= 4:
@@ -145,24 +150,11 @@ def periodic_inference_helper(device_mac_addr, data):
                         matched = 1
                         
                         cur_domain_set.remove(j)
-                        print('Remove : ',j)
-                        print(cur_domain_set)
                 if matched == 1:
                     cur_domain_set.remove(i)
-                    print('Remove : ',i)
                     cur_domain_set.add('*.'+suffix)
 
-        print('Protocol %s, domain set:' % cur_protocol)
-        print(f'Current Domain Set: {cur_domain_set}')
-
-        '''
-        Jakaria: I am not sure what is the purpose of this code
-        Please check the code and update the comment
-        '''
         for cur_domain in cur_domain_set:
-
-            print('Protocol %s, domain: %s' % (cur_protocol,cur_domain))
-
             domain_count = {}
             count_dic ={}
             cur_feature = []
@@ -196,15 +188,11 @@ def periodic_inference_helper(device_mac_addr, data):
                     filter_feature.append(False)
             cur_feature = X_feature[filter_feature]
             
-            print('Domain count flow:', domain_count)
             domain_count2 = len(count_dic.keys())
 
-            print('Domain count unique block:', domain_count2)
             if count_dic == {}:
                 continue
-            # if domain_count[cur_domain] <= 10:
-            #     continue
-            
+
             '''
             min time = start time
             '''
@@ -221,26 +209,9 @@ def periodic_inference_helper(device_mac_addr, data):
             x = list(map(lambda x:x-x_min_tmp,x))
             y = list(requestOrdered.values())
 
-            # create a folder for each device
-            os.makedirs('%s/%s' % (file_path, device_mac_addr.replace(':', '_')), exist_ok=True)
+            # create a folder for storing the periodicity inference result
+            os.makedirs('%s' % (file_path), exist_ok=True)
 
-            """
-            Plot time domain 
-            """
-            plt.figure()
-            plt.plot(x, y) #
-            plt.grid()
-            plt.xlabel('time')
-            plt.ylabel('volume')
-            plt.yscale("log")
-            plt.title('%s' % device_mac_addr)
-            # todo Jakaria replaced name to remove error due to windows system
-            plt.savefig('%s/%s/%s_%s.png' % (file_path,
-                                             device_mac_addr.replace(':', '_'),
-                                             cur_domain.replace(':', '_').replace('*', '_'),
-                                             cur_protocol.replace(':', '_')))
-            # plt.show()
-            plt.close()
             count=0
             time_list =  []
             if domain_count2 < 30:
@@ -254,52 +225,22 @@ def periodic_inference_helper(device_mac_addr, data):
             """
             # N = 800
             N = len(x)  # number of signal points
-            print('N:', N)
             # sample spacing
             # T = 1.0 / ( N) 
             T = N / N
             # sampling frequency 
             f_s = 1/T
-            
             yf = fft(y)
-            xf = fftfreq(N, T)[:N//2]
-            tmp_max = np.max(np.abs(yf[0:N//2]))
-            tmp_mean = np.mean(np.abs(yf[0:N//2]))
-            tmp_std = np.std(np.abs(yf[0:N//2]))
-            print(tmp_max, tmp_mean,tmp_std)
-            
-            """
-            We use DFT to extract candidate periods for a group by identifying the frequencies that carry significant power in spectral density. The period candidates are the inverse of significant frequencies. To discover the significance in spectral density, a threshold need to be set. We apply an approach from prior work to generate random permutations of the input sequence that do not have any periodicity. The maximum power of each permutation can be used as an indicator of aperiodicity.
-            """ 
-            """
-            @inproceedings{li2010mining,
-            title={Mining periodic behaviors for moving objects},
-            author={Li, Zhenhui and Ding, Bolin and Han, Jiawei and Kays, Roland and Nye, Peter},
-            booktitle={Proceedings of the 16th ACM SIGKDD international conference on Knowledge discovery and data mining},
-            pages={1099--1108},
-            year={2010}
-            }
-            @inproceedings{vlachos2005periodicity,
-            title={On periodicity detection and structural periodic similarity},
-            author={Vlachos, Michail and Yu, Philip and Castelli, Vittorio},
-            booktitle={Proceedings of the 2005 SIAM international conference on data mining},
-            pages={449--460},
-            year={2005},
-            organization={SIAM}
-            }
-            """
+
             # permutation 100 times to set threshold
             p_max_list = []
             for i in range(100):
                 y_shuffle = np.random.permutation(y).tolist()
                 p_max_list.append(np.max(np.abs(fft(y_shuffle)[1:N//2]).tolist()))
             threshold_99 = sorted(p_max_list)[-6]
+
             if sampling_rate >= 600:
                 threshold_99 = sorted(p_max_list)[-11]
-                print('Threshold 90 percentile: ', threshold_99)
-            else:
-                print('Threshold 95 percentile: ', threshold_99)
-            
             
             tmp_list = []
             tmp_list_yf = []
@@ -310,12 +251,6 @@ def periodic_inference_helper(device_mac_addr, data):
                     tmp_list.append(i)
                     tmp_list_yf.append(np.abs(yf[i]))
 
-            print('List len > threshold:', len(tmp_list))
-            print('yf index: ', tmp_list[:10])
-            print('yf: ', tmp_list_yf[:10])
-            
-            print('zipped:', sorted(list(zip(tmp_list,tmp_list_yf)), key = lambda x:x[1], reverse = True)[:5])
-        
             period = []
             period_tmp_list = []
             if len(tmp_list) >0:
@@ -328,12 +263,13 @@ def periodic_inference_helper(device_mac_addr, data):
                             if round(N/tmp_list[i]) != period[-1]:
                                 period.append(round(N/tmp_list[i]))
                                 period_tmp_list.append(tmp_list[i])
-            
-            print('Protocol %s, domain: %s' % (cur_protocol,cur_domain))
-            print('period:',period[:])
+        
             
             """
-            Then, we use autocorrelation to validate the period candidates and identify the true period for each pattern. The periods that have a significant autocorrelation score are chosen as the final periods of the signal. 
+            Then, we use autocorrelation to validate the period candidates and 
+            identify the true period for each pattern. The periods that have a
+            significant autocorrelation score are chosen as the final periods
+            of the signal. 
             """
             acf = sm.tsa.acf(y, nlags=len(y),fft=True)
         
@@ -355,10 +291,6 @@ def periodic_inference_helper(device_mac_addr, data):
                         j-=1
                 autocorrelation = set(autocorrelation)
                 autocorrelation = sorted(autocorrelation,key=lambda x:x[1], reverse = True)
-                if len(autocorrelation) > 20:
-                    print('## Autocorrelation ',autocorrelation[:20])
-                else:
-                    print('## Autocorrelation ',autocorrelation)
             
             # special case that has only few data points: 
             if not any(autocorrelation) and domain_count2 <= 6 and domain_count2 >= 4:
@@ -368,23 +300,8 @@ def periodic_inference_helper(device_mac_addr, data):
                 res = [x for x in diff_diff if x <= 3600/sampling_rate]
                 if len(res)==len(diff_diff):
                     autocorrelation.append((np.mean(time_diff),0))
-                    print('## Less than 6 data points: period ',autocorrelation)
-                # print(time_diff)
 
-
-            """
-            Plot frequency domain
-            """
-            plt.figure()
-            plt.plot(xf, 1.0/N * np.abs(yf[0:N//2]))
-            plt.grid()
-            plt.savefig('%s/%s/%s_%s_fft.png' % (file_path,
-                                                 device_mac_addr.replace(':', '_'),
-                                                 cur_domain.replace(':', '_').replace('*', '_'),
-                                                 cur_protocol.replace(':', '_')))
-            plt.close()
-
-            print('--------------------------------------------------------')
+            # print('--------------------------------------------------------')
             
             with open('%s/%s.txt' % (file_path, device_mac_addr.replace(':', '_')), 'a+') as file:
                     if len(period) > 0 and any(autocorrelation): # and len(acf_burst) > 1
