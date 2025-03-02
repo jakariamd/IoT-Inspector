84053f0c1d01096028f5c000502cfee331bebcbc
Md Jakaria <jakaria.dcc11@gmail.com>
2025-03-02 12:45:00 -0500

    pre-process feature added

---
+++ core/preprocess_feature.py
@@ -0,0 +1,13 @@
+'''
+Title: Preprocess Feature
+Author: Md Jakaria
+Date: Feb 2025
+Description: This module preprocesses the feature of idle device. 
+Features are standardized and normalized.
+Reference: This code was inspired by and adapted from the work of Tianrui Hue
+on https://github.com/NEU-SNS/BehavIoT/tree/main.
+'''
+
+def preprocess_feature():
+    
+    pass
