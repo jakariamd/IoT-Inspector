9c12acb28ed6b38593d140d6ebb9b8c6484eec42
Md Jakaria <jakaria.dcc11@gmail.com>
2025-03-09 22:00:11 -0400

    Auto-commit

--- core/model_selection.py
+++ core/model_selection.py
@@ -1,3 +1,8 @@
 # This files aims to provide a set of functions to 
 # perform model selection for a device based on the
 # data available in the database.
+
+def import_models():
+    # Import all models from the models directory
+    # and return them as a list of models
+    pass
